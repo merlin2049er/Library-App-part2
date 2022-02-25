0a2cb2400e0b3a7968310e89067b9689e48ce987
Joe Guerra <merlin2049er@gmail.com>
2022-02-25 11:18:05 -0500

    ..

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,7 +84,7 @@ config.action_mailer.smtp_settings = {
   address:               "smtp.gmail.com",
   port:                  587,
   user_name:            "JGuerra@jginfosys.com",
-  password:             "wfbjousprwucxaqf",
+  password:             "",
   authentication:       "plain",
   enable_starttls_auto: true
 }
